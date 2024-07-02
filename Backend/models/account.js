27631c52e589fd6f31926dcd7e9016385f0a72fc
suttonj07 <suttonj07@gmail.com>
2024-07-01 20:18:51 -0400

    created server js/models/controllers

---
+++ Backend/models/account.js
@@ -0,0 +1,29 @@
+Event.init({
+    event_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    start_time: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    end_time: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Event',
+    tableName: 'events',
+    timestamps: false
+  })
+  return Event
---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+// DEPENDENCIES
+const express = require('express')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// ROOT
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to Somnus Sleep Tracker'
+    })
+})
+// Routes
+
+const accountsController = require('./controllers/accounts_controller')
+// app.use('/account', accountsController)
+
+const sleepController = require('./controllers/sleep_controller')
+// app.use('/sleep', sleepController)
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+  })
+  
